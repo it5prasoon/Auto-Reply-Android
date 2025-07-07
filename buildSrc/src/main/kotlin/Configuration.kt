a154df256db6191ab45472d47e0ca0b8a645ae28
it5prasoon <prasoonk187@gmail.com>
2025-07-07 20:14:25 +0530

    updated version code

--- buildSrc/src/main/kotlin/Configuration.kt
+++ buildSrc/src/main/kotlin/Configuration.kt
@@ -6,6 +6,6 @@ object Configuration {
     private const val minorVersion = 25
     private const val patchVersion = 1
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-    const val versionCode = 27
+    const val versionCode = 28
     const val artifactGroup = "com.matrix.autoreply"
 }
